43294af052edbb1a9dc1febaaf79302c0347f736
Eurípedes <euripedes.naves@outlook.com>
2022-10-01 16:19:38 -0300

    Expected IRR seted

--- src/components/Simulator.js
+++ src/components/Simulator.js
@@ -1,34 +1,43 @@
 
 // Variáveis Formulário
-const rendaMensal = 0;
-const montaDespesa = 0;
-const anosAposentadoria = 0;
-const RendaEsperada = 0;
+const rendaMensal = 8000;
+const montaDespesa = 4000;
+const anosAposentadoria = 30;
+const RendaEsperada = 18000;
 
 // Proporção perfil de investimento
 const rendaFixa = 0;
-const rendaVariavel = 0;
+const rendaVariavel = 1;
 
 
 // Dados de mercado - API futura
-const taxaRendaFixa = 0;
-const taxaRendaVariavel = 0;
+const taxaRendaFixa = 2.3;
+const taxaRendaVariavel = 6.3;
 
 //Dados após aposentadoria
-const taxaAposentadoria = 0;
+const taxaAposentadoria = 5.5;
 
 // Resultados preliminares
 const superavitEsperado = rendaMensal - montaDespesa;
 
-//TIR Esperada 
+
 
 
 // Taxa Equivalente
 function iq(taxa) {
     return (
-        (((1+(taxa/100)) ** (1/12)) - 1)
+        (((1 + (taxa / 100)) ** (1 / 12)) - 1)
     )
 }
 
-console.log(iq(2.3));
+// TIR Esperada
 
+function expectedIRR() {
+    let rendimentoRendaFixa = iq(taxaRendaFixa) * (rendaFixa * superavitEsperado);
+    let rendimentoRendaVariavel = iq(taxaRendaVariavel) * (rendaVariavel * superavitEsperado);
+    let expectedIRR = (((((rendimentoRendaFixa + rendimentoRendaVariavel) / superavitEsperado) + 1) ** 12) - 1);
+    
+    return (expectedIRR)
+}
+
+console.log(expectedIRR());
